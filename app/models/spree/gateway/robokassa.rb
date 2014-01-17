d712a90689ad4d11f1f0d4d371d78861e8f988c7
Alexander Osmanov <grandison@mail.ru>
2014-01-17 17:09:55 +0400

    Fix robokassa

--- app/models/spree/gateway/robokassa.rb
+++ app/models/spree/gateway/robokassa.rb
@@ -4,7 +4,7 @@ module Spree
     preference :password2, :string
     preference :mrch_login, :string
 
-    attr_accessible :preferred_password1, :preferred_password2, :preferred_mrch_login
+    # attr_accessible :preferred_password1, :preferred_password2, :preferred_mrch_login
 
     def provider_class
       self.class
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   namespace :gateway do
-    match '/robokassa/:gateway_id/:order_id' => 'robokassa#show',    :as => :robokassa
-    match '/robokassa/result'                => 'robokassa#result',  :as => :robokassa_result
-    match '/robokassa/success'               => 'robokassa#success', :as => :robokassa_success
-    match '/robokassa/fail'                  => 'robokassa#fail',    :as => :robokassa_fail
+    get '/robokassa/:gateway_id/:order_id' => 'robokassa#show',    :as => :robokassa
+    get '/robokassa/result'                => 'robokassa#result',  :as => :robokassa_result
+    get '/robokassa/success'               => 'robokassa#success', :as => :robokassa_success
+    get '/robokassa/fail'                  => 'robokassa#fail',    :as => :robokassa_fail
   end
 
 end
